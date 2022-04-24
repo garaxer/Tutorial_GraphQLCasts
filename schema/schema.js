7985702ed7eb6b7b4e35f087a2006a735faaf152
Gary B <gbagnall8@gmail.com>
2022-04-24 12:06:34 +1000

    schema changes

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,21 @@
 // What property each object has and how they are related to eachother (building your own ORM)
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require("lodash");
+const axios = require("axios");
 
 const users = [
   { id: "1", firstName: "Gary", age: 20 },
   { id: "11", firstName: "Gary2", age: 21 },
 ];
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -14,6 +23,7 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: { type: CompanyType },
   },
 });
 
@@ -24,9 +34,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, {
-          id: args.id,
-        });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
