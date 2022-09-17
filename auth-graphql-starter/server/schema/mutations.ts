f8f72467531b5367483252fb8df8638fd5d6e669
Gary B <gbagnall8@gmail.com>
2022-09-17 21:42:03 +1000

    Add root query

--- auth-graphql-starter/server/schema/mutations.ts
+++ auth-graphql-starter/server/schema/mutations.ts
@@ -1,6 +1,6 @@
 import { GraphQLObjectType, GraphQLString } from "graphql";
 import UserType from "./types/user_type";
-import { signup } from "../services/auth";
+import { signup, login } from "../services/auth";
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -15,7 +15,49 @@ const mutation = new GraphQLObjectType({
         return signup({ email, password, req });
       },
     },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout(function (err) {
+          // if (err) { return next(err); }
+          // res.redirect('/');
+          console.log(err);
+        });
+
+        return user;
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {
+        return login({ email, password, req });
+      },
+    },
   },
 });
 
 export default mutation;
+
+
+// mutation {
+//   signup(email: "test", password: "testP") {
+//     email
+//   }
+// }
+
+// mutation {
+//   logout {
+//     email
+//   }
+// }
+
+// mutation {
+//   login(email: "test", password: "testP") {
+//     email
+//   }
+// }
--- auth-graphql-starter/server/schema/types/root_query_type.ts
+++ auth-graphql-starter/server/schema/types/root_query_type.ts
@@ -1,10 +1,23 @@
 import { GraphQLObjectType, GraphQLID } from "graphql";
+import UserType from "./user_type";
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    dummyField: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        // If the user is null, then they are not authenticated
+        return req.user;
+      },
+    },
   },
 });
 
+// Example query
+// {
+//   user {
+//     email
+//   }
+// }
 export default RootQueryType;
